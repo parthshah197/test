a1d0bdc0b88362f69611540776dfbe1d78b5cee1
parthshah197 <parthshah197@gmail.com>
2017-03-30 13:45:45 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/prati/courses/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.prati.courses;
+
+
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+public class MainActivity extends ActionBarActivity {
+    ArrayList<String> listItems=new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    Spinner sp;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sp=(Spinner)findViewById(R.id.spinner);
+        adapter=new ArrayAdapter<String>(this,R.layout.spinner_layout,R.id.txt,listItems);
+        sp.setAdapter(adapter);
+
+    }
+    public void onStart(){
+        super.onStart();
+        BackTask bt=new BackTask();
+        bt.execute();
+    }
+    private class BackTask extends AsyncTask<Void,Void,Void> {
+        ArrayList<String> list;
+        protected void onPreExecute(){
+            super.onPreExecute();
+            list=new ArrayList<>();
+        }
+        protected Void doInBackground(Void...params){
+            InputStream is=null;
+            String result="";
+            try{
+                HttpClient httpclient=new DefaultHttpClient();
+                HttpPost httppost= new HttpPost("http://192.168.43.55/course.php");
+                HttpResponse response=httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                // Get our response as a String.
+                is = entity.getContent();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+            //convert response to string
+            try{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    result+=line;
+                }
+                is.close();
+                //result=sb.toString();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            // parse json data
+            try{
+                JSONArray jArray =new JSONArray(result);
+                for(int i=0;i<jArray.length();i++){
+                    JSONObject jsonObject=jArray.getJSONObject(i);
+                    list.add(jsonObject.getString("name"));
+                }
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+        protected void onPostExecute(Void result){
+            listItems.addAll(list);
+            adapter.notifyDataSetChanged();
+        }
+    }
+}
